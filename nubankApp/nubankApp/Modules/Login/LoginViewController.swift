d48f094494f4faa2f08c9469dc4284c56120b23c
gabriel <Gabriel.Paschoal-ext@ab-Inbev.com>
2022-10-28 19:34:39 -0300

    Created the app core

---
+++ nubankApp/nubankApp/Modules/Login/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  nubankApp
+//
+//  Created by Gabriel Paschoal on 28/10/22.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+    }
+}
