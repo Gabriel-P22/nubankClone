13777f52f3bd15d3da4f22dfd2e56a0281135c0c
gabriel <Gabriel.Paschoal-ext@ab-Inbev.com>
2022-10-28 21:25:36 -0300

    Created dependency injection inside the app

---
+++ nubankApp/nubankApp/DI/AppContainer.swift
@@ -0,0 +1,16 @@
+//
+//  AppContainer.swift
+//  nubankApp
+//
+//  Created by Gabriel Paschoal on 28/10/22.
+//
+
+import Foundation
+import Swinject
+
+class AppConainter {
+    public static let shared: Container = {
+        let container = Container()
+        return container
+    }()
+}
